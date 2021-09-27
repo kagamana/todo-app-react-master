dd3a6c00b413939124bf31f59f08351b0fe9322c
Thong <choimiki212@gmail.com>
2021-09-27 15:54:39 +0700

    thong

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import Button from "@atlaskit/button";
 function App(){
   return (
     <>
-      <div>
-        <p>Danh sách công việc cần thực hiện trong ngày hôm nay</p>
-        <TextField name='add-todo' placeholder='Thêm việc cần làm ...' elemAfterInput={<Button isDisabled={false} appearance='primary'>Thêm</Button>} css={{ padding: '2px 4px 8px'}}></TextField>
-        <TodoList/>
-      </div>
+      <p>Danh sách công việc cần thực hiện trong ngày hôm nay</p>
+      <TextField
+        name='add-todo'
+        placeholder='Thêm việc cần làm ...'
+        elemAfterInput={
+          <Button isDisabled={false} appearance='primary'>
+            Thêm
+          </Button>
+        }
+        css={{ padding: "2px 4px 2px" }}
+      ></TextField>
+      <TodoList/>
     </>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,11 @@
-import React from 'react'
-import Button from '@atlaskit/button'
+import React from 'react';
+import Button from '@atlaskit/button';
+import styled from 'styled-components';
+
+const ButtonStyled = styled(Button)`
+    margin-top: 5px;
+    text-align: left;
+`;
 export default function Todo() {
-    return <Button shouldFitContainer>Item 1</Button>
+    return <ButtonStyled shouldFitContainer>Item 1</ButtonStyled>
 }
