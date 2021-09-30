762838e779c3a84f2f7ab9d6fcf26261af5e08b5
Thong <choimiki212@gmail.com>
2021-09-30 23:39:53 +0700

    thong

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import TodoList from "./components/TodoList";
 import TextField from "@atlaskit/textfield";
 import Button from "@atlaskit/button";
-import { useState } from "react";
-import { v4 } from 'uuid';
-import { useCallback } from "react";
+import { useState, useCallback } from "react";
+import { v4 } from "uuid";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -13,25 +12,43 @@ function App() {
     setTextInput(e.target.value);
   }, []);
 
-  const onAddBtnClick = useCallback((e) => {
-    setTodoList([{ id: v4(), name: textInput, isComplete: false}, ...todoList]);
-    setTextInput("");
-  }, [textInput, todoList]);
+  const onAddBtnClick = useCallback(
+    (e) => {
+      setTodoList([
+        { id: v4(), name: textInput, isComplete: false },
+        ...todoList,
+      ]);
+      setTextInput("");
+    },
+    [textInput, todoList]
+  );
+
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList((prevState) =>
+      prevState.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
+  }, []);
   return (
     <>
       <p>Danh sách công việc cần thực hiện trong ngày hôm nay</p>
       <TextField
-        name='add-todo'
-        placeholder='Thêm việc cần làm ...'
+        name="add-todo"
+        placeholder="Thêm việc cần làm ..."
         elemAfterInput={
-          <Button isDisabled={!textInput} appearance='primary' onClick={onAddBtnClick}>
+          <Button
+            isDisabled={!textInput}
+            appearance="primary"
+            onClick={onAddBtnClick}
+          >
             Thêm công việc
           </Button>
         }
         value={textInput}
         onChange={onTextInputChange}
       ></TextField>
-      <TodoList todoList={todoList}/>
+      <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick} />
     </>
   );
 }
