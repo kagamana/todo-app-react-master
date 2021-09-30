b964b9853b9e33488aeef1979b88ab66d9b7674d
Thong <choimiki212@gmail.com>
2021-09-30 23:56:18 +0700

    thong

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import TodoList from "./components/TodoList";
 import TextField from "@atlaskit/textfield";
 import Button from "@atlaskit/button";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { v4 } from "uuid";
+
+const TODO_APP_STORAGE_KEY = "TODO_APP";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [textInput, setTextInput] = useState("");
+
+  useEffect(() => {
+    const storagedTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
+    if (storagedTodoList) {
+      setTodoList(JSON.parse(storagedTodoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
+  }, [todoList]);
 
   const onTextInputChange = useCallback((e) => {
     setTextInput(e.target.value);
